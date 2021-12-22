3c6206de12618bf408a30ed6b57aa632dc85760d
Usman Khan <uk03777@gmail.com>
2021-12-22 23:08:51 +0500

    Added some more features

--- index.js
+++ index.js
@@ -9,3 +9,22 @@ open.addEventListener('click',function open() {
 close.addEventListener('click', function close() {
     navbar.style.right = '-200%'
 })
+
+
+// Modal making
+const envelope = document.querySelector('.envelope')
+const modal = document.querySelector('.modal');
+const send = document.querySelector('.send')
+const remove = document.querySelector('.remove')
+
+
+envelope.addEventListener('click', function () {
+    modal.style.opacity = "100%"
+})
+
+remove.addEventListener('click', function () {
+    modal.style.opacity = "0%"
+})
+send.addEventListener('click', function() {
+    modal.style.opacity = '0%'
+})
